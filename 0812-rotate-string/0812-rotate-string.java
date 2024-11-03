5ec1f7d8827b2ff685fa0642ab9f98cc2514d9f6
Solaiman Hossain <101270007+imsnto@users.noreply.github.com>
2024-11-03 06:37:15 +0600

    Time: 0 ms (100%), Space: 41.6 MB (27.67%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length() > goal.length()) return false;
         String t = s.concat(s);
-        return t.indexOf(goal) >= 0;
+        return s.length() == goal.length() && t.contains(goal) ;
     }
 }
