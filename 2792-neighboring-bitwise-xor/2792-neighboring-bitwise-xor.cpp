1798aa8afdd6be6c3cf4a7aa721cb39b54758775
Solaiman Hossain <101270007+imsnto@users.noreply.github.com>
2025-01-17 10:30:17 +0600

    Time: 29 ms (9.35%), Space: 289.9 MB (5.61%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -1,7 +1,20 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& d) {
-        int ans = accumulate(d.begin(), d.end(), 0);
-        return ans % 2 ==  0;
+        vector<int> org {1};
+        int n = d.size();
+
+        for(int i=1; i<n; i++){
+            org.push_back(d[i-1] ^ org.back());
+        }
+        if(org.back() ^ d[n-1] == org[0]) return true;
+
+        org = {0};
+             for(int i=1; i<n; i++){
+            org.push_back(d[i-1] ^ org.back());
+        }
+        if(org.back() ^ d[n-1] == org[0]) return true;
+
+        return false;
     }
 };
