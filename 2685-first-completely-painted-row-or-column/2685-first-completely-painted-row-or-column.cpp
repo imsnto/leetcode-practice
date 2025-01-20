3b300582eca0484bdd99e6fa4b1fb609a480d687
Solaiman Hossain <101270007+imsnto@users.noreply.github.com>
2025-01-20 08:16:09 +0600

    Time: 88 ms (75.3%), Space: 170.5 MB (48.48%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -3,7 +3,7 @@ public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         int n = mat.size(), m = mat[0].size();
         vector<int> row(n), col(m);
-        map<int, pair<int,int>> mp;
+        unordered_map<int, pair<int,int>> mp;
 
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
@@ -15,8 +15,6 @@ public:
         for(int i=0; i<x; i++){
             pair<int,int> p = mp[arr[i]];
          
-
-
             if(++row[p.first] == m) return i;
             if(++col[p.second] == n) return i;
             
