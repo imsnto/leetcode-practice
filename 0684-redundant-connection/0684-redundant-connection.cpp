04e56e882fd1d2f2b56df0ce2fa67f73c5aacb01
Solaiman Hossain <101270007+imsnto@users.noreply.github.com>
2025-01-29 14:03:23 +0600

    Time: 8 ms (15.35%), Space: 38.1 MB (5.18%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -3,19 +3,16 @@ public:
     int a, b;
     bool dfs(int v, int par, vector<set<int>>&g, vector<int>&vis){
         vis[v] = 1;
-        cout << v << " " << par << endl;
 
         bool ans = false;
         for(int child: g[v]){
             if(child == par) continue;
             if(vis[child]){
-                cout << child << " " << par << " " << v << endl;
                 ans = true;
             } else {
                  ans |= dfs(child, v, g, vis);
             }
         }
-        //cout << par << " " << v << endl;
         return ans;
     }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
@@ -23,14 +20,12 @@ public:
         vector<set<int>> g(n+1);
         for(auto e: edges){
              g[e[0]].insert(e[1]);
-                g[e[1]].insert(e[0]);
+            g[e[1]].insert(e[0]);
             vector<int>vis(n+1);
             if(dfs(e[0], -1, g, vis) ) {
                 return e;
             }
-            else{
-               
-            }
+       
         }
 
         return {};
