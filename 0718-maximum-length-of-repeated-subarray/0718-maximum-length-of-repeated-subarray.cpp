a614b3a12c740930bf2c569efb1638e99552a8af
Solaiman Hossain <101270007+imsnto@users.noreply.github.com>
2025-02-06 15:36:21 +0600

    Time: 248 ms (14.49%), Space: 89.6 MB (11.08%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
+int mx ;
+    int mlrs(int i, int j, vector<int>&nums1, vector<int>&nums2, vector<vector<int>>&memo){
+        if(i<0 or j<0) return 0;
 
-    int findLength(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size(), m = nums2.size();
-        vector<vector<int>> memo(n+1,vector<int>(m+1));
+        
+        int &ans = memo[i][j];
+        if(ans != -1) return ans;
 
-        int ans = 0;
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(nums1[i-1] == nums2[j-1]){
-                    memo[i][j] = 1 + memo[i-1][j-1];
-                    ans = max(ans, memo[i][j]);
-                }
-            }
-        }
-
+        ans = 0;
+        if(nums1[i] == nums2[j])  ans = 1+mlrs(i-1, j-1, nums1, nums2, memo);
+        mlrs(i-1, j, nums1, nums2, memo) ;
+        mlrs(i, j-1, nums1, nums2, memo);
+        mx = max(mx, ans);
         return ans;
+
+    }
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size(), m = nums2.size(); mx = 0;
+        vector<vector<int>> memo(n,vector<int>(m, -1));
+
+         mlrs(n-1, m-1, nums1, nums2, memo);
+         return mx;
     }
 };
