cc62f75a6dfd6063d0a2571e81fd5f53e1f36d3a
Solaiman Hossain <101270007+imsnto@users.noreply.github.com>
2025-02-06 15:57:58 +0600

    Time: 27 ms (70.96%), Space: 31.4 MB (58.64%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
+int lcp(int i, int j, string& s, vector<vector<int>>&memo){
+        if(i>j) return 0;
+        if(i == j) return 1;
+
+        int &ans = memo[i][j];
+        if(ans != -1) return ans;
+
+        if(s[i] == s[j]) return ans = 2 + lcp(i+1, j-1, s, memo);
+        return ans = max(lcp(i+1, j, s, memo), lcp(i, j-1, s, memo));
+    }
+    
     int minInsertions(string s) {
-        string t = s;
-        reverse(t.begin(), t.end());
-        int n = s.size(), m = t.size();
-        vector<vector<int>>dp(n+1, vector<int>(m+1));
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(s[i] == t[j]){
-                    dp[1][j+1] = dp[0][j] + 1;
-                }else{
-                    dp[1][j+1] = max(dp[1][j], dp[0][j+1]);
-                }
-            }
-            dp[0] = dp[1];
-        }
-        return n - dp[1][m];
+                int n = s.size();
+        vector<vector<int>> memo(n, vector<int>(n, -1));
+
+        return n - lcp(0, n-1, s, memo);
+       
     }
 };
