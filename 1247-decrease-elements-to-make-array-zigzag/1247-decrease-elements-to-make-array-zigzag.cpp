4ec86854138b9881d72e5fbf587cf66ab61b50f6
Solaiman Hossain <101270007+imsnto@users.noreply.github.com>
2025-04-13 10:06:33 +0600

    Time: 0 ms (100%), Space: 10 MB (38.31%) - LeetHub

--- 1247-decrease-elements-to-make-array-zigzag/1247-decrease-elements-to-make-array-zigzag.cpp
+++ 1247-decrease-elements-to-make-array-zigzag/1247-decrease-elements-to-make-array-zigzag.cpp
@@ -1,42 +1,14 @@
 class Solution {
 public:
-    int toZigzag(vector<int> nums){
-        int count = 0;
-        for(int i=1; i<nums.size(); i+=2){
-         if(i==nums.size()-1){
-                if(nums[i] >= nums[i-1]){
-                    count += nums[i] - nums[i-1] + 1;
-                }
-            }
-            else if(nums[i] < nums[i-1] and nums[i] < nums[i+1]); 
-       
-            else{
-                int mn = min(nums[i-1], nums[i+1]);
-                count += (nums[i] - mn) + 1;
-            }
-        }
-        int count2 = 0;
-        for(int i=0; i<nums.size(); i+=2){
-            if(i==0){
-                if(nums[i] >= nums[i+1]) {
-                    count2 += nums[i] - nums[i+1] + 1;
-                }
-            }
-            else if(i == nums.size()-1){
-                if(nums[i] > nums[i-1]){
-                    count2 += nums[i]-nums[i-1] + 1;
-                }
-            }
-            else if(nums[i] < nums[i-1] and nums[i] < nums[i+1]); 
-            else{
-                int mn = min(nums[i-1], nums[i+1]);
-                count2 += (nums[i] - mn) + 1;
-            }
-        }
-        return min(count, count2);
-    }
+
     int movesToMakeZigzag(vector<int>& nums) {
-        if(nums.size() == 1) return 0;
-        return toZigzag(nums);
+        int res[2] = {0, 0}, left, right, n = nums.size();
+        for(int i=0; i<n; i++){
+            left = i>0 ? nums[i-1] : 10001;
+            right = i+1 < n ? nums[i+1] : 10001;
+
+            res[i%2] += max(0, nums[i] - min(left, right) + 1);
+        }
+        return min(res[0], res[1]);
     }
 };
