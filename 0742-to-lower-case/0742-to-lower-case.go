2e6e0f33fac8ad73af2cb6c47d4939b9bff0cf2c
Solaiman Hossain <101270007+imsnto@users.noreply.github.com>
2025-07-02 06:57:21 +0600

    Time: 0 ms (100%), Space: 4.1 MB (35.38%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.go
+++ 0742-to-lower-case/0742-to-lower-case.go
@@ -1,3 +1,9 @@
 func toLowerCase(s string) string {
-    return strings.ToLower(s)
+    r := []rune(s)
+    for idx, char := range s {
+        if char >= 'A' && char <= 'Z' {
+            r[idx] = char + ('a' - 'A')
+        }
+    }
+    return string(r)
 }
