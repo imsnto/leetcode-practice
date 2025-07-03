735eb32fbba09bd993bf117b795eedc49896d5d1
Solaiman Hossain <101270007+imsnto@users.noreply.github.com>
2025-07-03 17:54:33 +0600

    Time: 9 ms (24.14%), Space: 6.8 MB (32.76%) - LeetHub

---
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.go
@@ -0,0 +1,16 @@
+func kthCharacter(k int) byte {
+    s := []rune{'a'}
+    for len(s) < k {
+        nextS := make([]rune, len(s))
+        for i, char := range s {
+            if char == 'z' {
+                nextS[i] = 'a'
+            } else {
+                nextS[i] = char + 1
+            }
+        }
+        s = append(s, nextS...)
+    }
+    fmt.Println(s)
+    return byte(s[k-1])
+}
