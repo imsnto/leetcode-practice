6f1797b3af7ff4cd45c9087a16229ca1bf54fbf5
Solaiman Hossain <101270007+imsnto@users.noreply.github.com>
2025-07-17 10:34:34 +0600

    Time: 3 ms (81.89%), Space: 11.8 MB (87.91%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    bool helper(int idx, vector<int>&nums, vector<int>&equal, int sum){
-        if(idx< 0){
-            int c = 0;
-            for(int &i: equal) c += sum == i;
-            return c == (int)equal.size();
+    int sum = 0;
+    bool helper(int idx, vector<int>&equal, vector<int>&nums, int k){
+        if(idx < 0 ) {
+            int d = 0;
+            for(int i: equal) 
+                d += i == sum;
+            return d == k;
         }
-        for(int j=0; j<equal.size(); j++){
-            if(nums[idx] + equal[j] <= sum){
-            equal[j] += nums[idx];
-            bool f = helper(idx-1, nums, equal, sum);
-            if(f) return f;
-            equal[j] -= nums[idx];
+        for(int i=0; i<k; i++){
+            if(equal[i] + nums[idx] <= sum ) {
+                equal[i] += nums[idx];
+                bool f = helper(idx-1, equal, nums, k);
+                if(f) return f;
+                equal[i] -= nums[idx];
             }
         }
         return false;
     }
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        int n = nums.size();
+        int total = 0, n = nums.size();
+        for(int i: nums) total += i;
+        sum = total / k;
+        if(total % k != 0) return false;
         vector<int> equal(k);
-        int sum = 0;
-        for(int i: nums) sum += i;
-        if(sum % k != 0) return false;
-        sum /= k;
+        int i= 0;
         sort(nums.begin(), nums.end());
-        int i = 0;
         for(int j=n-1; j>n-k; j--) equal[i++] = nums[j];
-        return helper(n-k, nums, equal, sum);
+        return helper(n-k, equal, nums, k);
     }
 };
